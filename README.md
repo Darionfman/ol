831952cee8351c5f8726e9c098057fdf0f9ca480
Darion Freeman <darionfman@gmail.com>
2016-12-28 18:41:55 -0600

    Updates test and adds a readme

---
+++ README.md
@@ -0,0 +1,68 @@
+#OL
+
+This app is a business directory that uses React/Mobx/Webpack
+for the client side and Express/Node for the server side and 
+calls on a restfull api for JSON data.
+
+##Install
+
+First copy the repo link and clone it down
+
+``
+git clone https://github.com/Darionfman/ol.git
+``
+Once the repo is cloned down install dependancies
+
+For npm
+
+``
+npm install
+``
+
+For yarn
+
+``
+yarn
+``
+##Build
+Once the dependancies are installed input the bundling command
+
+For npm
+
+``
+npm run build
+``
+
+For yarn
+
+``
+yarn build
+``
+##Start
+Once the scripts are bundled run the start command
+
+For npm
+
+``
+npm run start
+``
+
+For yarn
+
+``
+yarn start
+``
+##Test
+This app uses Jest for the testing suite
+to run test simply run the test command
+For npm
+
+``
+npm run test
+``
+
+For yarn
+
+``
+yarn test
+``
--- __test__/Directory.spec.js
+++ __test__/Directory.spec.js
@@ -11,13 +11,13 @@ test('Directory renders with correct number of children', async () => {
   )
   let tree = component.toJSON()
   //should have all 50 businesses as child nodes
-  expect(tree.children.length).toBe(50)
+  expect(tree.children[1].children.length).toEqual(50)
   //child nodes should be contained in divs
-  expect(tree.children[0].type).toBe('div')
+  expect(tree.children[1].children[0].type).toBe('div')
   //they should be full width collumns
-  expect(tree.children[12].props.className).toBe('col-md-12')
+  expect(tree.children[1].children[12].props.className).toBe('col-md-12')
   //the second child of the child nodes should have a child containing text
-  expect(typeof tree.children[12].children[0].children[1].children[0]).toBe('string')
+  expect(typeof tree.children[1].children[12].children[0].children[1].children[0]).toBe('string')
   //checks to make sure it matches the recent snapshot
   expect(tree).toMatchSnapshot()
 })
